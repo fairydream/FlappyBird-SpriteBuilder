366bfc46af0db969fbcb36b2067ec5018fcf514e
fairydream <fairydream523@126.com>
2015-01-24 16:31:59 -0800

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -10,6 +10,7 @@
     character = (Character*)[CCBReader load:@"Character"];
     [physicsNode addChild:character];
     [self addObstacle];
+    timeSinceObstacle = 0.0f;
 }
 
 -(void)update:(CCTime)delta
@@ -34,7 +35,7 @@
 - (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
     // this will get called every time the player touches the screen
     [character flap];
-    timeSinceObstacle = 0.0f;
+    
 }
 
 @end
